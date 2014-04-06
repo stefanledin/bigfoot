41a5f97ffce3df8f8767e9e0b5ae41a404f7b37d
stefanledin <stefanledin@gmail.com>
2014-04-06 02:15:26 +0200

    Removed everything but the theme. From now on, this is just a starter theme and not a boilerplate for an entire project.

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-define('WP_USE_THEMES', true);
-require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Bigfoot</title>
+	</head>
+	<body <?php body_class();?>>
+		<?php if (have_posts()) : ?>
+			<?php while(have_posts()) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</body>
+</html>
