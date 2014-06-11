4b51ca192612bb46958013c91f9e20a3d39adad3
stefanledin <stefanledin@gmail.com>
2014-06-11 14:27:53 +0200

    Added $themeDir

--- includes/enqueue-scripts-styles.php
+++ includes/enqueue-scripts-styles.php
@@ -1,6 +1,7 @@
 <?php
 function enqueue_scripts_and_styles () {
-	$assetsDir = get_template_directory_uri() . '/assets';
+	$themeDir = get_template_directory_uri();
+	$assetsDir = $themeDir . '/assets';
 	$assetsPath = get_stylesheet_directory() . '/assets';
 	
 	// wp_enqueue_style( $handle, $src, $deps, $ver, $media );
